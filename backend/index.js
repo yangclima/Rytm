d700cb0511e3ad92ad482184f76234542403a638
andsongui25 <andson.sguimaraes@ufpe.br>
2025-09-03 02:31:24 -0300

    Adciona rota 'PUT /taks' para permitir atualização das tasks

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 
-import { getTasks } from "./infra/db.js";
+import { getTasks, updateTasks } from "./infra/db.js";
 
 const app = express();
 
@@ -15,6 +15,17 @@ app.use(express.json());
 app.get("/tasks", async (req, res) => {
     const tasks = await getTasks();
     return res.status(200).json(tasks)
+})
+
+app.put("/tasks", async (req, res) => {
+    try {
+        const { tasksToUpdate } = req.body;
+        await updateTasks(tasksToUpdate);
+    } catch (err) {
+        return res.status(500).json({ message: "Internal server error"})
+    } finally {
+        return res.status(200).json({ message: "Resource updated successfully"})
+    }
 })
 
 const PORT = 3030
--- backend/infra/db.js
+++ backend/infra/db.js
@@ -6,3 +6,16 @@ export async function getTasks() {
     const tasks = await prisma.task.findMany();
     return tasks
 }
+
+export async function updateTasks(tasksToUpdate) {
+    await tasksToUpdate.map(async (task) => {
+        console.log(task)
+        await prisma.task.update({
+            where: {id: task.id},
+            data: {
+                remainingTime: task.remainingTime,
+                isDone: task.isDone,
+            }
+        })
+    })
+}
